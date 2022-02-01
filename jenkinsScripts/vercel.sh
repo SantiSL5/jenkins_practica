94e7cb7ebf777f9e6bd5e6db85a63c66538e507c
SantiSL5 <santiagosolerllin@gmail.com>
2022-02-01 20:16:05 +0100

    vercel 7

--- jenkinsScripts/vercel.sh
+++ jenkinsScripts/vercel.sh
@@ -1,5 +1,5 @@
 if [ $1 -eq 0 ] && [ $2 -eq 0 ] && [ $3 -eq 0 ] && [ $4 -eq 0 ]; then
-    vercel . --token $5 --confirm --name santi_jenkins
+    vercel . --token $5 --confirm --name santi-jenkins
     exit 0
 else
     exit 1
